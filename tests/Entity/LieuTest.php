f16e9b4d2b9e4a59bb972472719985ffd1e878e5
Yannick Maury <yannick.maury@iepg.fr>
2020-03-03 10:07:32 +0100

    test Lieu setter and getter

--- tests/Entity/LieuTest.php
+++ tests/Entity/LieuTest.php
@@ -21,4 +21,21 @@ class LieuTest extends TestCase {
         $this->assertInstanceOf(Lieu::class, $lieu);
     }
 
+    public function testLieuNom()
+    {
+        $this->lieu->setNom('Grenoble');
+        $this->assertEquals('Grenoble', $this->lieu->getNom());
+    }
+
+    public function testLieuLongitude()
+    {
+        $this->lieu->setLongitude(45.188258);
+        $this->assertEquals(45.188258, $this->lieu->getLongitude());
+    }
+    
+    public function testLieuLatitude()
+    {
+        $this->lieu->setLatitude(5.725154);
+        $this->assertEquals(5.725154, $this->lieu->getLatitude());
+    }
 }
