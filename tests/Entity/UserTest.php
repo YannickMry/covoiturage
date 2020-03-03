f6d27b7da19241bad5169c6259acda93c1a4c2f8
Yannick Maury <yannick.maury@iepg.fr>
2020-03-03 09:53:26 +0100

    test all setter and getter entity User

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -21,4 +21,33 @@ class UserTest extends TestCase {
         $this->assertInstanceOf(User::class, $user);
     }
 
+    public function testUserNom()
+    {   
+        $this->user->setNom('Martin');
+        $this->assertEquals('Martin', $this->user->getNom());
+    }
+
+    public function testUserPrenom()
+    {
+        $this->user->setPrenom('Philippe');
+        $this->assertEquals('Philippe', $this->user->getPrenom());
+    }
+
+    public function testUserEmail()
+    {
+        $this->user->setEmail('martin.philippe@iut2.fr');
+        $this->assertEquals('martin.philippe@iut2.fr', $this->user->getEmail());
+    }
+
+    public function testUserUsername()
+    {
+        $this->user->setUsername('martinp');
+        $this->assertEquals('martinp', $this->user->getUsername());
+    }
+
+    public function testUserPassword()
+    {
+        $this->user->setPassword('test');
+        $this->assertEquals('test', $this->user->getPassword());
+    }
 }
