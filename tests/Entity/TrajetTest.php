df901de1465f1e570f89fa07394d2e35635ffef9
Yannick Maury <yannick.maury@iepg.fr>
2020-03-03 10:29:33 +0100

    test Trajet setter and getter

--- tests/Entity/TrajetTest.php
+++ tests/Entity/TrajetTest.php
@@ -2,9 +2,12 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Lieu;
 use PHPUnit\Framework\TestCase;
 use App\Entity\Trajet;
-
+use App\Entity\User;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class TrajetTest extends TestCase {
 
@@ -21,4 +24,54 @@ class TrajetTest extends TestCase {
         $this->assertInstanceOf(Trajet::class, $trajet);
     }
 
+    public function testTrajetLieuDepart()
+    {
+        $lieu = new Lieu();
+        $this->trajet->setLieuDepart($lieu);
+        $this->assertInstanceOf(Lieu::class, $this->trajet->getLieuDepart());
+    }
+
+    public function testTrajetLieuArrivee()
+    {
+        $lieu = new Lieu();
+        $this->trajet->setLieuArrivee($lieu);
+        $this->assertInstanceOf(Lieu::class, $this->trajet->getLieuArrivee());
+    }
+
+    public function testTrajetDateDepart()
+    {
+        $date = new \DateTime();
+        $this->trajet->setDateDepart($date);
+        $this->assertInstanceOf(\DateTime::class, $this->trajet->getDateDepart());
+    }
+
+    public function testTrajetPlaces()
+    {
+        $this->trajet->setPlaces(6);
+        $this->assertEquals(6, $this->trajet->getPlaces());
+    }
+
+    public function testTrajetConducteur()
+    {
+        $conducteur = new User();
+        $this->trajet->setConducteur($conducteur);
+        $this->assertInstanceOf(User::class, $this->trajet->getConducteur());
+    }
+
+    public function testTrajetPassagers()
+    {
+        $passager = new User();
+        $passager->setNom('passager1');
+
+        $passager2 = new User();
+        $passager2->setNom('passager2');
+
+        $this->trajet->addPassager($passager);
+        $this->trajet->addPassager($passager2);
+        
+        $this->assertInstanceOf(ArrayCollection::class, $this->trajet->getPassagers());
+        foreach ($this->trajet->getPassagers() as $passager) {
+            $this->assertInstanceOf(User::class, $passager);
+        }
+    }
 }
