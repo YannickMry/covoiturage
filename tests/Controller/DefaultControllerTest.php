3fb8e7be02189c3a10e067211f9b986445bbfe5e
Yannick Maury <yannick.maury@iepg.fr>
2020-03-03 13:36:31 +0100

    test are displayed all links

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -13,4 +13,43 @@ class DefaultControllerTest extends WebTestCase {
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    public function testHomepageLinkConnexion()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $link = $crawler->selectLink('Connexion')->link();
+        
+        $client->click($link);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals('/login', $client->getRequest()->getPathInfo());
+    }
+
+    public function testHomepageLinkRegister()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $link = $crawler->selectLink('Sign up')->link();
+        
+        $client->click($link);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals('/register', $client->getRequest()->getPathInfo());
+    }
+
+    public function testHomepageLinkResearchTrajet()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $link = $crawler->selectLink('Rechercher trajet')->link();
+        
+        $client->click($link);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals('/research-trajet', $client->getRequest()->getPathInfo());
+    }
 }
