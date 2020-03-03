b0f8118e037513030f40f3fd54395e197157fba7
Yannick Maury <yannick.maury@iepg.fr>
2020-03-03 10:56:14 +0100

    test Security login and logout

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase {
+
+    public function testLogin()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testLogout()
+    {
+        $client = static::createClient();
+        $client->followRedirects();
+
+        $client->request('GET', '/logout');
+
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
